b10c8ec57144a055b0a5ace791a640f5475da928
Simon <61758347+SimDebug27@users.noreply.github.com>
2025-03-21 17:06:55 +0100

    Added file
    Added code for webscrapping qoutes to scrape

---
+++ web_scrapping.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+page_to_scrape = requests.get("https://quotes.toscrape.com/")
+soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+
+quotes = soup.find_all("span", class_="text")
+authors = soup.find_all("small", class_="author")
+tags = soup.find_all("div", class_="tags")
+
+dataQuotes = []
+
+for quote, author, tag in zip(quotes, authors, tags):
+    dataQuotes.append([quote.text, author.text])
+
+df = pd.DataFrame(dataQuotes, columns=['Quotes', 'Authors'])
+
+df.to_csv('qoutes.csv', index=False, sep="\t", encoding='utf-8')
